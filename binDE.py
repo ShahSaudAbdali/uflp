83467b44eeb28f9b3d0b9e5335afa1ca592a8ff8
Shah Saud Abdali <shahsaudabdali@gmail.com>
2022-12-08 16:11:11 +0530

    transfer function updated

--- binDE.py
+++ binDE.py
@@ -63,6 +63,9 @@ print("Facility Open :", listOfOpeningFacility)
 print("Serving Cost  :", servingCostSum)
 print("Serving Facility:", servingFacilityIndices)
 
+def obj(x):
+    return x[0]**2.0 + x[1]**2.0
+
 def fitness(candidate, facilityToLocationList, openingCostFacility):
     candidate = transferFunctions.F1(candidate)         #calling transfer function F1
     b_candidate = binarization.M1(candidate)            #calling binarization method 1
@@ -89,66 +92,66 @@ def fitness(candidate, facilityToLocationList, openingCostFacility):
     return (openingCost + totalServingCost)
 
 # # define mutation operation
-# def mutation(x, F):
-#     return x[0] + F * (x[1] - x[2])
-#
+def mutation(x, F):
+    return x[0] + F * (x[1] - x[2])
+
 # # define boundary check operation
-# def check_bounds(mutated, bounds):
-#     mutated_bound = [clip(mutated[i], bounds[i, 0], bounds[i, 1]) for i in range(len(bounds))]
-#     return mutated_bound
+def check_bounds(mutated, bounds):
+    mutated_bound = [clip(mutated[i], bounds[i, 0], bounds[i, 1]) for i in range(len(bounds))]
+    return mutated_bound
 #
 # # define crossover operation
-# def crossover(mutated, target, dims, cr):
-#     p = rand(dims)
-#     trial = [mutated[i] if p[i] < cr else target[i] for i in range(dims)]       #generate trial vector
-#     return trial
-#
-# def differential_evolution(pop_size, bounds, iter, F, cr):
-#     # initialise population of candidate solutions randomly within the specified bounds
-#     pop = bounds[:, 0] + (rand(pop_size, len(bounds)) * (bounds[:, 1] - bounds[:, 0]))
-#     ##print(pop)
-#     obj_all = 0 #[obj(ind) for ind in pop]     #initial vector
-#     # find the best performing vector of    initial population
-#     best_vector = pop[argmin(obj_all)]
-#     best_obj = min(obj_all)
-#     prev_obj = best_obj
-#     # initialise list to store the objective function value at each iteration
-#     obj_iter = list()
-#     # run iterations of the algorithm
-#     for i in range(iter):
-#         # iterate over all candidate solutions
-#         for j in range(pop_size):
-#             # choose three candidates, a, b and c, that are not the current one
-#             candidates = [candidate for candidate in range(pop_size) if candidate != j]
-#             a, b, c = pop[choice(candidates, 3, replace=False)]
-#             # perform mutation
-#             mutated = mutation([a, b, c], F)
-#             # check that lower and upper bounds are retained after mutation
-#             mutated = check_bounds(mutated, bounds)
-#             # perform crossover
-#             trial = crossover(mutated, pop[j], len(bounds), cr)
-#
-#             ########################Work On this####################################
-#             ##obj_target = obj(pop[j])          #calculate obj func for target vector
-#             ##obj_trial = obj(trial)              #calculate obj func for trial vector
-#             # perform selection
-#             # if obj_trial < obj_target:
-#             #     # replace the target vector with the trial vector
-#             #     pop[j] = trial
-#             #     # store the new objective function value
-#             #     obj_all[j] = obj_trial
-#             ##########################################################################
-#
-#         #best performing vector at each iteration
-#         best_obj = min(obj_all)
-#         # store the lowest objective function value
-#         if best_obj < prev_obj:
-#             best_vector = pop[argmin(obj_all)]
-#             prev_obj = best_obj
-#             obj_iter.append(best_obj)
-#             # report progress at each iteration
-#             print('Iteration: %d f([%s]) = %.5f' % (i, around(best_vector, decimals=5), best_obj))
-#     return [best_vector, best_obj, obj_iter]
+def crossover(mutated, target, dims, cr):
+    p = rand(dims)
+    trial = [mutated[i] if p[i] < cr else target[i] for i in range(dims)]       #generate trial vector
+    return trial
+
+def differential_evolution(pop_size, bounds, iter, F, cr):
+    # initialise population of candidate solutions randomly within the specified bounds
+    pop = bounds[:, 0] + (rand(pop_size, len(bounds)) * (bounds[:, 1] - bounds[:, 0]))
+    ##print(pop)
+    obj_all = [obj(ind) for ind in pop]     #initial vector
+    # find the best performing vector of    initial population
+    best_vector = pop[argmin(obj_all)]
+    best_obj = min(obj_all)
+    prev_obj = best_obj
+    # initialise list to store the objective function value at each iteration
+    obj_iter = list()
+    # run iterations of the algorithm
+    for i in range(iter):
+        # iterate over all candidate solutions
+        for j in range(pop_size):
+            # choose three candidates, a, b and c, that are not the current one
+            candidates = [candidate for candidate in range(pop_size) if candidate != j]
+            a, b, c = pop[choice(candidates, 3, replace=False)]
+            # perform mutation
+            mutated = mutation([a, b, c], F)
+            # check that lower and upper bounds are retained after mutation
+            mutated = check_bounds(mutated, bounds)
+            # perform crossover
+            trial = crossover(mutated, pop[j], len(bounds), cr)
+
+            ########################Work On this####################################
+            obj_target = obj(pop[j])          #calculate obj func for target vector
+            obj_trial = obj(trial)              #calculate obj func for trial vector
+            # perform selection
+            if obj_trial < obj_target:
+                # replace the target vector with the trial vector
+                pop[j] = trial
+                # store the new objective function value
+                obj_all[j] = obj_trial
+            ##########################################################################
+
+        #best performing vector at each iteration
+        best_obj = min(obj_all)
+        # store the lowest objective function value
+        if best_obj < prev_obj:
+            best_vector = pop[argmin(obj_all)]
+            prev_obj = best_obj
+            obj_iter.append(best_obj)
+            # report progress at each iteration
+            print('Iteration: %d f([%s]) = %.5f' % (i, around(best_vector, decimals=5), best_obj))
+    return [best_vector, best_obj, obj_iter]
 
 #Applying DE to the transformed binary array
 pop_size = 10
@@ -157,6 +160,6 @@ iter = 100                      #No of iterations for crossover
 F = 0.5                         #SF for mutation
 cr = 0.7                        #Cross Over Rate
 
-# solution = differential_evolution(pop_size, bounds, iter, F, cr)
-#print('\nSolution: f([%s]) = %.5f' % (around(solution[0], decimals=5), solution[1]))
+solution = differential_evolution(pop_size, bounds, iter, F, cr)
+print('\nSolution: f([%s]) = %.5f' % (around(solution[0], decimals=5), solution[1]))
 
--- transferFunctions.py
+++ transferFunctions.py
@@ -1,5 +1,7 @@
 #Transfer Functions Definitions: F1-F3 S-shaped, F4-F8 V-Shaped
 import math
+import numpy
+import random
 
 def F1(vector):
     for index, arr in enumerate(vector):
@@ -18,8 +20,44 @@ def F3(vector):
         vector[index] = 1 / (1 + math.exp(-arr * SLOPE))
     return vector
 
-def F2(vector):
+def F4(vector):
     SLOPE = 1
     for index, arr in enumerate(vector):
         vector[index] = math.tanh(-arr * SLOPE)
     return vector
+
+
+def S1(x):
+    return 1 / (1 + numpy.exp(-(2 * x)))
+
+
+def S2(x):
+    return 1 / (1 + numpy.exp(-x))
+
+
+def S3(x):
+    return 1 / (1 + numpy.exp(-(x / 2)))
+
+
+def S4(x):
+    return 1 / (1 + numpy.exp(-(x / 3)))
+
+
+def V1(x):
+    VectorLength = len(x)
+    y = [0] * VectorLength
+    for i in range(VectorLength):
+        y[i] = abs(math.erf(x[i]))
+    return y
+
+
+def V2(x):
+    return abs(numpy.tanh(x))
+
+
+def V3(x):
+    return abs(x / numpy.sqrt(1 + x * x))
+
+
+def V4(x):
+    return abs((2 / math.pi) * numpy.arctan((math.pi / 2) * x))
