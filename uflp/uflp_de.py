e136ce4cfed9407bc7330f596b3a25cb674b1a1e
Shah Saud Abdali <shahsaudabdali@gmail.com>
2023-08-14 15:06:30 +0530

    fitness function updated

--- uflp/uflp_de.py
+++ uflp/uflp_de.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-def uflp_objective(solution, customers, facilities, f_cost, c_cost):
+def uflp_objective(solution, f_cost, c_cost):
     total_cost = np.sum(c_cost * solution) + np.sum(f_cost * (np.sum(solution, axis=1) > 0))
     return total_cost
 
 def differential_evolution_uflp(customers, facilities, f_cost, c_cost, population_size=50, max_generations=100, F=0.8, CR=0.5):
-    # Initialize population randomly
-    population = np.random.randint(2, size=(population_size, facilities, customers))
+    # Initialize population randomly, Xi
+    population = np.random.randint(2, size=facilities)
 
     for gen in range(max_generations):
         for i in range(population_size):
@@ -19,14 +19,14 @@ def differential_evolution_uflp(customers, facilities, f_cost, c_cost, populatio
             trial_solution = np.where(crossover_mask, v, population[i])
 
             # Evaluate fitness
-            trial_fitness = uflp_objective(trial_solution, customers, facilities, f_cost, c_cost)
-            current_fitness = uflp_objective(population[i], customers, facilities, f_cost, c_cost)
+            trial_fitness = uflp_objective(trial_solution, f_cost, c_cost)
+            current_fitness = uflp_objective(population[i], f_cost, c_cost)
 
             # Selection
             if trial_fitness < current_fitness:
                 population[i] = trial_solution
 
-    best_solution = population[np.argmin([uflp_objective(sol, customers, facilities, f_cost, c_cost) for sol in population])]
+    best_solution = population[np.argmin([uflp_objective(sol, f_cost, c_cost) for sol in population])]
     return best_solution
 
 # Main Function Call
@@ -57,10 +57,11 @@ if __name__ == "__main__":
 
     f_cost = np.array(listOfOpeningFacility)
     c_cost = np.array(costFacilityToLocation)
+    c_cost = c_cost.transpose()
 
     print("Total Facility: ", facilities, "Total Customers: ", customers)
-    print("Facility Opening Cost Array: \n", f_cost)
+    print("Facility Opening Cost Array: ", f_cost)
     print("Customer to Facility Cost Array: \n", c_cost)
     best_solution = differential_evolution_uflp(customers, facilities, f_cost, c_cost)
-    # print("Best facility assignment to customers:\n", best_solution)
-    # print("Total cost of the solution:", uflp_objective(best_solution, customers, facilities, f_cost, c_cost))
+    print("Best facility assignment to customers:\n", best_solution)
+    # print("Total cost of the solution:", uflp_objective(best_solution, f_cost, c_cost))
